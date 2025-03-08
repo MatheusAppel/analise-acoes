24872112869b77bc17a9e8c133e0a940b3731633
Matheus Appel <matheus.appel@hotmail.com>
2025-03-08 17:35:56 -0300

    Ajustando configuração para build no Render

---
+++ build.sh
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Instala dependências do sistema necessárias
+apt-get update -y
+apt-get install -y python3-dev gcc build-essential
+
+# Atualiza pip e instala dependências Python
+python -m pip install --upgrade pip
+pip install --no-cache-dir -r requirements.txt
